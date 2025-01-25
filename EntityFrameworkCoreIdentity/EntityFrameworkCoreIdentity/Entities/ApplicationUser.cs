aa2118eb31a4efe70c63c59505f7f7688176f42a
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-01-25 14:53:02 -0300

    feat: create entities

---
+++ EntityFrameworkCoreIdentity/EntityFrameworkCoreIdentity/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameworkCoreIdentity.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    [Column("USER_RG")]  
+    public string RG { get; set; } = string.Empty;
+}
---
+++ EntityFrameworkCoreIdentity/EntityFrameworkCoreIdentity/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace EntityFrameworkCoreIdentity.Entities;
+
+public class Product
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
