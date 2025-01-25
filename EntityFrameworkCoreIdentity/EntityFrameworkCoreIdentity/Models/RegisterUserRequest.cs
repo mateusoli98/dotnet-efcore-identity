e4dd131e8991131b2bf90e89f82ff142a4606ad3
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-01-25 14:55:51 -0300

    feat: add models

---
+++ EntityFrameworkCoreIdentity/EntityFrameworkCoreIdentity/Models/RegisterUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFrameworkCoreIdentity.Models;
+
+public class RegisterUserRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string RG { get; set; } = string.Empty;
+}
